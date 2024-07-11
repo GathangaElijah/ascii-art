d672701c70062ee511f95e3a61aa19845304e052
egathang <elyg3672@gmail.com>
2024-07-11 18:08:55 +0300

    Style: Removing comments

--- asciimodifier/asciigraphics.go
+++ asciimodifier/asciigraphics.go
@@ -1,18 +1,15 @@
 package asciimodifier
 
 import (
-	"fmt"
 	"strings"
 )
 
-// import "fmt"
 
 func AsciiGraphics(inputString string, asciimap map[rune][]string) string {
 	// var data string
 	var output strings.Builder
 	var temp [][]string
 	splitString := strings.Split(inputString, "\\n")
-	fmt.Println("This is my splitted string=> ", splitString)
 	for _, words := range splitString {
 		if len(words) == 0 {
 			output.WriteString("\n")
@@ -23,8 +20,7 @@ func AsciiGraphics(inputString string, asciimap map[rune][]string) string {
 				temp = append(temp, asciiRep)
 			}
 		}
-		fmt.Println("This is the length of words => ", len(words))
-		fmt.Println("Length of temp => ", len(temp))
+		
 		for i := 0; i < 8; i++ {
 			for j := 0; j < len(words); j++ {
 				output.WriteString(temp[j][i])
