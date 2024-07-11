26008a366cd4823276245f92ad51da4e4659b6c7
egathang <elyg3672@gmail.com>
2024-07-11 17:33:29 +0300

    Feat: Handling the newlines

--- asciimodifier/asciigraphics.go
+++ asciimodifier/asciigraphics.go
@@ -1,31 +1,40 @@
 package asciimodifier
 
+import (
+	"fmt"
+	"strings"
+)
+
 // import "fmt"
 
 func AsciiGraphics(inputString string, asciimap map[rune][]string) string {
 	// var data string
+	var output strings.Builder
 	var temp [][]string
-	for _, char := range inputString {
-		temp = append(temp, asciimap[char])
-	}
-	word := ""
-	for i := 0; i < 8; i ++ {
-		for j := 0; j < len(inputString); j++ {
-			word += temp[j][i]
+	splitString := strings.Split(inputString, "\\n")
+	fmt.Println("This is my splitted string=> ", splitString)
+	for _, words := range splitString {
+		if len(words) == 0 {
+			output.WriteString("\n")
+			continue
 		}
-		if i != 7 {
-			word+= "\n"
+		for _, char := range words {
+			if asciiRep, ok := asciimap[char]; ok {
+				temp = append(temp, asciiRep)
+			}
 		}
-
-
+		fmt.Println("This is the length of words => ", len(words))
+		fmt.Println("Length of temp => ", len(temp))
+		for i := 0; i < 8; i++ {
+			for j := 0; j < len(words); j++ {
+				output.WriteString(temp[j][i])
+			}
+			if i < 8 {
+				output.WriteString("\n")
+			}
+		}
+		temp = nil
 	}
-	// for _, char := range inputString {
-	// 	if graphicRep, ok := asciimap[char]; ok {
-	// 		fmt.Println(string(graphicRep))
-	// 		// data +=  string(graphicRep)+"\n"
-	// 	}
-	// }
 
-
-	return word
+	return output.String()
 }
